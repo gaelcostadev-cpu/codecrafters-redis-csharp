476e0e29bde0dec54a24bcea6f2550a35aa09c14
gaelcostadev-cpu <gaelcostadev@gmail.com>
2026-04-13 08:06:51 -0300

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -5,23 +5,33 @@ using System.Text;
 TcpListener server = new(IPAddress.Any, 6379);
 server.Start();
 
-List<Socket> clients = [server.AcceptSocket()];
+List<Socket> clients = [];
 
 // 1024 bytes pode ser qualquer valor, dependendo do tamanho da msg que você espera receber
 byte[] msg = new byte[1024]; 
 
 while (true)
 {
+    //enquanto houver uma conexão pendente, aceita e adiciona a lista de clientes
+    if (server.Pending()) clients.Add(server.AcceptSocket());
+    
     for (int i = 0; i < clients.Count; i++)
     {
         Socket client = clients[i];
+
+        //cliente desconectado, remove da lista
         if (!client.Connected)
         {
             clients.RemoveAt(i);
             i--;
             continue;
         }
-        client.Receive(msg);
-        client.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
+
+        //determina se o cliente tem dados para ler, se sim, lê a mensagem e responde com +PONG
+        if (client.Poll(0, SelectMode.SelectRead))
+        {
+            client.Receive(msg);
+            client.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
+        }
     }
 }
