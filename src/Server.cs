ae260893dbfa7f6e835a0abb1e8c9ae48bb5b6f2
gaelcostadev-cpu <gaelcostadev@gmail.com>
2026-04-13 07:19:23 -0300

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -6,7 +6,9 @@ TcpListener server = new(IPAddress.Any, 6379);
 server.Start();
 
 Socket client = server.AcceptSocket();
+byte[] buffer = new byte[1024];
 
-do {
+while (client.Connected) {
+    client.Receive(buffer);
     client.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
-} while (client.Connected);
+}  
